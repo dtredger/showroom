690712eabbcf63a9fb34bdf4881775ce6ce0b75b
jjcody <jackcody@gmail.com>
2014-03-23 23:00:55 -0700

    Added basic Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,6 @@ Showspace::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  config.action_mailer.default_url_options = { host: 'localhost.com:3000' }
 end
