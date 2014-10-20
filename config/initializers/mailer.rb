02cc21a04bc73837b78780ffba45726fb81371f3
dtredger <d.tredger@gmail.com>
2014-10-20 00:56:50 -0400

    creds for testing mail

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,9 +1,19 @@
-ActionMailer::Base.smtp_settings = {
-    port:           '587',
-    address:        'smtp.mandrillapp.com',
-    user_name:      ENV['MANDRILL_USERNAME'],
-    password:       ENV['MANDRILL_APIKEY'],
-    domain:         'heroku.com',
-    authentication: :plain,
-}
+# ActionMailer::Base.smtp_settings = {
+#   port:           '587',
+#   address:        'smtp.mandrillapp.com',
+#   user_name:      ENV['MANDRILL_USERNAME'],
+#   password:       ENV['MANDRILL_APIKEY'],
+#   domain:         'heroku.com',
+#   authentication: :plain,
+# }
 ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.smtp_settings = {
+	address: "smtp.gmail.com",
+	port: 587,
+	domain: "gmail.com",
+	user_name: "ENV['GMAIL_ADDR']",
+	password: ENV['GMAIL_PWD'],
+	authentication: "plain",
+	enable_starttls_auto: true
+}
