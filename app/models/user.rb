39c84d792908d2a06cf7497d5ab59fb32f2cbf16
dtredger <d.tredger@gmail.com>
2014-10-22 13:31:01 -0400

    don't add confirmable for now

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,8 +37,13 @@ class User < ActiveRecord::Base
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
+  devise :database_authenticatable,
+         :registerable,
+         :recoverable,
+         :rememberable,
+         :trackable,
+         :validatable,
+         :omniauthable, omniauth_providers: [:facebook]
 
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
