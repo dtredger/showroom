e79123dad9f8a470e1179d033103eccf4bd6501f
dtredger <d.tredger@gmail.com>
2014-10-28 16:27:33 -0400

    include new_user_password on login form, devise knows mailer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,26 @@ module ApplicationHelper
     end
   end
 
+  def user
+    @user ||= User.new
+  end
+
+  #
+  # def resource
+  #   @resource ||= User.new
+  # end
+  #
+  # def resource_name
+  #   :user
+  # end
+  #
+  # def resource_class
+  #   resource.class
+  # end
+  #
+  # def devise_mapping
+  #   @devise_mapping ||= Devise.mappings[:user]
+  # end
+
+
 end
