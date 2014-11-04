174abfe61f65fb4fa48e06ed41998d95617e4679
dtredger <d.tredger@gmail.com>
2014-11-04 12:56:47 -0500

    basic display of items in admin interface

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,16 +12,18 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
+      column do
+        panel "Recent Posts" do
+          ul do
+            Item.last(5).map do |item|
+              li link_to(item.product_name, admin_item_path(item))
+            end
+          end
+        end
+      end
+    end
+
 
     #   column do
     #     panel "Info" do
