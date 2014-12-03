84b7370145d322d5187551bbb0e9851dab5c278c
dtredger <d.tredger@gmail.com>
2014-12-03 00:53:42 -0500

    generate admin/duplicate_warning, prevent admin_user creation

---
+++ app/admin/duplicate_warning.rb
@@ -0,0 +1,26 @@
+ActiveAdmin.register DuplicateWarning do
+
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:permitted, :attributes]
+  #   permitted << :other if resource.something?
+  #   permitted
+  # end
+
+  controller do
+    def create
+      flash[:alert] = 'cats yo'
+      redirect_to root_path
+    end
+
+  end
+
+
+end
--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -5,7 +5,9 @@ ActiveAdmin.setup do |config|
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
   #
-  config.site_title = "Showspace"
+  config.site_title = 'Showspace'
+  config.site_title_link  = '/'
+  # config.site_title_image = "site_image.png"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
