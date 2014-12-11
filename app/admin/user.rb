ecdbc589aa7bfe8b869be4e901f73f08bead7bcc
dtredger <d.tredger@gmail.com>
2014-12-10 20:08:07 -0800

    blocks to reveal user item and closet count

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,18 +1,32 @@
 ActiveAdmin.register User do
 
+  permit_params :remember_created_at,
+    :sign_in_count,
+    :current_sign_in_at,
+    :last_sign_in_at,
+    :current_sign_in_ip,
+    :last_sign_in_ip,
+    :created_at,
+    :updated_at,
+    :fb_uid,
+    :fb_token,
+    :fb_token_expiration,
+    :username
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
 
+  index do
+    selectable_column
+    id_column
+    column :username
+    column :email
+    column :closets, lambda { |u| u.closets.count }
+    column :total_items_saved, lambda { |u| u.items.count }
+    column :created_at
+    column :updated_at
+    column :current_sign_in_at
+    column :current_sign_in_ip
+    column :sign_in_count
+    column :fb_token_expiration
+  end
 
 end
