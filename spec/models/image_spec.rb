2ff42df28bb5896e84fb961c82c6341792fe5778
dtredger <d.tredger@gmail.com>
2014-12-12 14:29:22 -0800

    image validates source + Item validity

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -5,29 +5,56 @@
 #  id         :integer          not null, primary key
 #  created_at :datetime
 #  updated_at :datetime
-#  image      :string(255)
+#  source     :string(255)
 #  item_id    :integer
 #
 
 require 'rails_helper'
 
 RSpec.describe Image, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
 
-#
-# context "images" do
-#   describe "image_source" do
-#     it "names image with filename" do
-#       expect(item_1["image_source"]).to eq("doge_log.gif")
-#     end
-#
-#     it "formats path with attributes" do
-#       expect(item_1.image_source.url).to eq("/items/a_store_na/designer_test/Test_Produ/doge_log.gif")
-#     end
-#
-#     it "stores CarrierWave file" do
-#       expect(item_1.image_source.file.class.to_s).to eq("CarrierWave::SanitizedFile")
-#     end
-#   end
-# end
+  SAMPLE_IMAGE_PATH = "#{Rails.root}/public/images/doge_log.gif"
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:user_2) { FactoryGirl.create(:user_2) }
+
+  let(:item) { FactoryGirl.create(:item) }
+
+
+  describe "validations" do
+    context "source" do
+      it "rejects blank" do
+        blank = item.images.build(source:nil)
+        expect(blank).to have(1).errors_on(:source)
+      end
+
+      it "errors on paths" do
+        expect{
+          item.images.build(source:SAMPLE_IMAGE_PATH)
+        }.to raise_error(CarrierWave::FormNotMultipart)
+      end
+
+      it "accepts files" do
+        file = item.images.build(source:open(SAMPLE_IMAGE_PATH))
+        expect(file).to be_valid
+      end
+    end
+
+    context "item" do
+      it "rejects for invalid item" do
+        invalid_item = FactoryGirl.build(:image,
+            source: open(SAMPLE_IMAGE_PATH),
+            item_id: 0)
+        expect(invalid_item).not_to be_valid
+      end
+
+      it "accepts for valid item" do
+        valid_item = FactoryGirl.build(:image,
+            source: open(SAMPLE_IMAGE_PATH),
+            item_id: item.id)
+        expect(valid_item).to be_valid
+      end
+    end
+  end
+
+end
