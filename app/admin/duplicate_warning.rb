a683bee89115308e443f5e0a1958ce11dd0b9055
dtredger <d.tredger@gmail.com>
2014-12-11 17:05:08 -0800

    ugly grid style for duplicate_warning shows images

--- app/admin/duplicate_warning.rb
+++ app/admin/duplicate_warning.rb
@@ -5,15 +5,31 @@ ActiveAdmin.register DuplicateWarning do
 
   permit_params :pending_item_id, :existing_item_id, :match_score, :warning_notes
 
+  index do
+    selectable_column
+    id_column
+    column :pending_item_id
+    column :existing_item_id
+    column :warning_notes
+    column :created_at
+    column :updated_at
+    actions
+  end
 
-  # index as: :grid, columns: 1 do |warning|
-  #   pending = Item.find(warning.pending_item_id)
-  #   existing = Item.find(warning.existing_item_id)
-  #   link_to image_tag(pending.image_source), admin_item_path(pending)
-  #   pending.product_name
-  #   link_to image_tag(existing.image_source), admin_item_path(existing)
-  #   existing.product_name
-  # end
+  index as: :grid, columns: 3 do |warning|
+    pending = Item.find(warning.pending_item_id)
+    existing = Item.find(warning.existing_item_id)
+    ul
+      li link_to image_tag(pending.images[0].image), admin_item_path(pending)
+      li pending.designer
+      li pending.product_name
+      li pending.store_name
+      br
+      li link_to image_tag(existing.images[0].image), admin_item_path(existing)
+      li existing.designer
+      li existing.product_name
+      li existing.store_name
+  end
 
 
   batch_action :delete_all_pending_items, priority: 1, confirm: "Delete Pending items?" do |ids|
