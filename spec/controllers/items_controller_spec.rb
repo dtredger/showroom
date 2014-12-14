ebdbcc62e99909131a7b1e3a32fc53c7c4533ae4
dtredger <d.tredger@gmail.com>
2014-12-14 11:37:28 -0800

    explicitly mention private methods verified in spec name

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -7,50 +7,50 @@ RSpec.describe ItemsController, :type => :controller do
   let!(:item3) { FactoryGirl.create(:unique_item, state: 1, category1: "test", price_cents: 300) }
   let!(:item4) { FactoryGirl.create(:unique_item) }
 
-  context "actions" do
-    describe "#index" do
-      context "default" do
-        before { get :index }
+  describe "#index" do
+    context "default" do
+      before { get :index }
 
-        it { expect(response.status).to eq(200) }
-        it("includes live @items") { expect(assigns[:items].length).to eq(3) }
-        it("includes correct items") { expect(assigns[:items]).to include(item1, item2, item3) }
-        it("does not contain pending items") { expect(assigns[:items]).not_to include(item4)}
-        it("builds closet item") { expect(assigns[:closets_item]).not_to be_nil }
+      it { expect(response.status).to eq(200) }
+      it("includes live @items") { expect(assigns[:items].length).to eq(3) }
+      it("includes correct items") { expect(assigns[:items]).to include(item1, item2, item3) }
+      it("does not contain pending items") { expect(assigns[:items]).not_to include(item4)}
+      it("builds closet item") { expect(assigns[:closets_item]).not_to be_nil }
+    end
+
+    context "#handle_search" do
+      describe "designer only" do
+        it "returns single match" do
+          get :index, designer: "lemon"
+          expect(assigns[:items].length).to eq(1)
+          expect(assigns[:items]).to include(item1)
+        end
       end
 
-      context "search" do
-        describe "designer only" do
-          it "returns single match" do
-            get :index, designer: "lemon"
-            expect(assigns[:items].length).to eq(1)
-            expect(assigns[:items]).to include(item1)
-          end
-        end
-
-        describe "category and price range" do
-          before { get :index, category1: "test", min_price: "2", max_price: "3" }
-          it("returns two matches") { expect(assigns[:items].length).to eq(2) }
-          it("includes correct items") { expect(assigns[:items]).to include(item2, item3) }
-          it("converts price param to cents") { expect(assigns[:items]).not_to include(item1) }
+      describe "category and price range" do
+        before { get :index, category1: "test", min_price: "2", max_price: "3" }
+        it("returns two matches") { expect(assigns[:items].length).to eq(2) }
+        it("includes correct items") { expect(assigns[:items]).to include(item2, item3) }
+        it "#value_to_cents converts price param to cents" do
+          expect(assigns[:items]).not_to include(item1)
         end
       end
     end
+  end
 
-    describe "#show" do
-      context "non-live item" do
-        before { get :show, id: item4.id }
-        it("redirects to index") { expect(response).to redirect_to(root_path) }
-      end
+  describe "#show" do
+    context "non-live item" do
+      before { get :show, id: item4.id }
+      it("redirects to index") { expect(response).to redirect_to(root_path) }
+    end
 
-      context "live item" do
-        before { get :show, id: item2.id }
+    context "live item" do
+      before { get :show, id: item2.id }
 
-        it { expect(response.status).to eq(200) }
-        it("assigns correct item") { expect(assigns[:item]).to eq(item2) }
-        it("builds closet item") { expect(assigns[:closets_item]).not_to be_nil }
-        it("builds like") { expect(assigns[:like]).not_to be_nil }
-      end
+      it { expect(response.status).to eq(200) }
+      it("assigns correct item") { expect(assigns[:item]).to eq(item2) }
+      it("builds closet item") { expect(assigns[:closets_item]).not_to be_nil }
+      it("builds like") { expect(assigns[:like]).not_to be_nil }
     end
   end
 
