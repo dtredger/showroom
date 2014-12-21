69015fad22639381abb10c84d6326e26e3fcbe93
dtredger <d.tredger@gmail.com>
2014-12-20 17:27:48 -0800

    login/sign-in and searches specs pass

---
+++ spec/requests/searches_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "Searches", :type => :request do
+  feature "nav-search" do
+    scenario "price range" do
+      create_full_items 10
+      visit root_path
+      fill_in "min_price", with: "500"
+      fill_in "max_price", with: "700"
+      click_button "Search"
+
+      expect(page).to have_content("$500")
+      expect(page).to have_content("$700")
+
+      expect(page).not_to have_content("$300")
+      expect(page).not_to have_content("$800")
+    end
+  end
+
+
+
+end
